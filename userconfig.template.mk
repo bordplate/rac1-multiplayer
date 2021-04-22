495276e2b6c351624b2a695f01083f0fe28d4744
TGEnigma <smartychrisje@hotmail.com>
2021-04-22 19:18:52 +0200

    fix userconfig template

--- userconfig.template.mk
+++ userconfig.template.mk
@@ -6,7 +6,7 @@ PATCH_FILE = $(RPCS3_DIR)\patches\patch.yml
 
 # hooks
 # todo move this shit elsewhere
-HOOK_RUNTIME_SUBSTITUTE_1 = runtimeSubstitute1/0xA3BE6C/"" # original is never called so no need to preserve the instruction
+HOOK_RUNTIME_SUBSTITUTE_1 = _runtimeSubstitute1/0xA3BE6C/"" # original is never called so no need to preserve the instruction
 HOOK_SET_BGM = setBgm/0x6CF04/"stdu  r1, -0x80(r1)"
 HOOK_SET_SEQ = setSeq/0x10DB4/"stdu  r1, -0xB0(r1)"
 HOOKS = $(HOOK_RUNTIME_SUBSTITUTE_1) $(HOOK_SET_BGM) $(HOOK_SET_SEQ)
