2d28d6f0ef6d74295a5b7cd6f5287a8c19303a5b
TGEnigma <smartychrisje@hotmail.com>
2021-04-30 14:02:00 +0200

    cleanup

--- prx/modules/test/testmodule.h
+++ prx/modules/test/testmodule.h
@@ -38,38 +38,5 @@ typedef struct exStruct
     exStruct2* structPointerValue; // this is how you define structure pointer
 } exStruct;
 
-
-// This is an example of how you declare prototypes of game functions in the header file.
-// In a prototype, you specify the signature of the function and you add documentation for other users
-// on how to use it as intended.
-
-/**
- * @brief Plays an SFX. aaaa
- * 
- * @param a0 a
- * @param a1 
- * @param a2 a
- * @param a3 a
- * @param a4 
- * @param a5 
- * @return s32 
- */
-s32 testModulePlaySfx( s32 a0, s32 a1, s32 a2, s32 a3, s32 a4, s32 a5 );
-
-/**
- * @brief Sets the next game sequence (AKA game mode/state)
- * 
- * @param seqId The ID of the sequence.
- * @param params Pointer to the param data.
- * @param paramsSize Size of the param data.
- * @param r6 
- * @return s32 
- */
-s32 testModuleSetSeq( s32 seqId, void* params, s32 paramsSize, s32 r6 );
-
-//SHK_FUNCTION_DEFINE_6( 0x10F0D4, s32, playSfx, s32, a0, s32, a1, s32, a2, s32, a3, s32, a4, s32, a5 );
-//SHK_FUNCTION_DEFINE_4( 0x10DB4, s32, setSeq, s32, seqId, void*, params, s32, paramsSize, s32, r6 );
-
-
 #endif // TESTMODULE_H
 #endif // GAME_TEST
