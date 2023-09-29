e1c178c7ce0895d642730671274307a77c6539ab
Panad <christophevandeneede@hotmail.com>
2023-09-29 13:27:07 +0200

    make even smaller part of packet and don't send

--- prx/rc1/multiplayer/Packet.cpp
+++ prx/rc1/multiplayer/Packet.cpp
@@ -190,7 +190,7 @@ Packet* Packet::make_collected_gold_bolt_packet(int bolt_number) {
 Packet* Packet::make_unlock_item_packet(int item_id) {
     Packet* packet = new Packet(sizeof(MPPacketSetState));
     packet->header->type = MP_PACKET_SET_STATE;
-    packet->header->size = sizeof(MPPacketSetState);
+//    packet->header->size = sizeof(MPPacketSetState);
 
 //    MPPacketSetState* body = (MPPacketSetState*)packet->body;
 //    body->state_type = MP_STATE_TYPE_UNLOCK_ITEM;
