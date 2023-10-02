77094c8db1cbfb6795e7784dd7dfbd5e6f91b7e0
Panad <christophevandeneede@hotmail.com>
2023-10-02 19:20:45 +0200

    debug statements

--- prx/rc1/multiplayer/GameClient.cpp
+++ prx/rc1/multiplayer/GameClient.cpp
@@ -252,7 +252,8 @@ void GameClient::update_set_state(MPPacketSetState* packet) {
             break;
         }
         case MP_STATE_TYPE_GIVE_BOLTS: {
-            Logger::debug("Changing bolt count, but we shouldn't be here right now.");
+            Logger::debug("Changing bolt count to: %d", (int)packet->value);
+            Logger::debug("Changing bolt count to: %lu", (unsigned long)packet->value);
             *(int*)0x969CA0 += (int)packet->value;
             break;
         }
