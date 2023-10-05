584547d514b2e75ea6b2a9f350fcfef840085bf3
Panad <christophevandeneede@hotmail.com>
2023-10-05 20:38:25 +0200

    Revert to official Directory Server ip

--- prx/rc1/Game.cpp
+++ prx/rc1/Game.cpp
@@ -216,7 +216,7 @@ int Game::query_servers_callback(void* data, size_t len, void* extra) {
 
 void Game::query_servers(int directory_id, ServerQueryCallback callback) {
     // client_ = new DirectoryClient("172.104.144.15", 2407);
-    client_ = new DirectoryClient("192.168.5.35", 2407);
+    client_ = new DirectoryClient("172.104.144.15", 2407);
     client_->_connect();
 
     server_query_callback_ = callback;
