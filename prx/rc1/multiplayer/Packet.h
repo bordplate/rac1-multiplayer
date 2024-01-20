1cf8a3433b51128cbaadc659467158edcd75c368
Panad <christophevandeneede@hotmail.com>
2024-01-20 10:08:50 +0100

    changed set_hud_text packet flags now also contain a bit specifying if the gamestate flags have been set. If the server is an older version, this bit will be 0 and we revert to the original behavior of only showing in the PlayerControl state

--- prx/rc1/multiplayer/Packet.h
+++ prx/rc1/multiplayer/Packet.h
@@ -203,8 +203,6 @@ typedef struct {
     float size;
     char text[50];
 } MPPacketSetHUDText;
-static u16 game_state_flags = 0xFF;
-static u16 game_state_offset = 0x2;
 
 // Packet used to query a directory for game servers
 typedef struct {
