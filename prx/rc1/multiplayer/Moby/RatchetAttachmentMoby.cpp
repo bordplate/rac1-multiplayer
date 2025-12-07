c09fbef2e65ea6e99609f57b1de805d6d2632921
bordplate <vetle@bordplate.no>
2025-12-07 22:49:51 +0100

    Set Pilots Helmet p_update to null when we make it a synced moby. Pilots Helmet moby is the same for the pickup on Gaspar and as equipped item and doesn't expect to have p_update be run when equipped. This fixes issue related to equipping it and picking it up on Gaspar.

--- prx/rc1/multiplayer/Moby/RatchetAttachmentMoby.cpp
+++ prx/rc1/multiplayer/Moby/RatchetAttachmentMoby.cpp
@@ -10,7 +10,13 @@
 #include "rc1/multiplayer/Player.h"
 
 SyncedMoby* RatchetAttachmentMoby::make_synced_moby(Moby* moby, u8 position_bone, u8 transform_bone) {
-    Logger::info("Making backpack moby");
+    Logger::info("Making Ratchet attachment moby");
+
+    if (moby->o_class == 1290) {
+        // Pilot's Helmet has an update func that it uses only on Gaspar for its pickup, so we null it here to make
+        //  so we don't run it either.
+        moby->p_update = nullptr;
+    }
 
     SyncedMoby* synced_moby = SyncedMoby::make_synced_moby(
         moby,
